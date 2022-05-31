bfc62f7f4648d86ba7b5b10967c7f237b1029810
Erhan Özgüç <eozguc@hotmail.com>
2022-05-31 10:48:43 +0300

    Add files via upload

---
+++ Grading/GradingStudents.cs
@@ -0,0 +1,54 @@
+﻿namespace Grading;
+
+public class GradingStudents
+{
+    public static void Main(string[] args)
+    {
+        TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
+
+        int gradesCount = Convert.ToInt32(Console.ReadLine().Trim());
+
+        List<int> grades = new List<int>();
+
+        for (int i = 0; i < gradesCount; i++)
+        {
+            int gradesItem = Convert.ToInt32(Console.ReadLine().Trim());
+            grades.Add(gradesItem);
+        }
+
+        List<int> result = Result.gradingStudents(grades);
+
+        textWriter.WriteLine(String.Join("\n", result));
+
+        textWriter.Flush();
+        textWriter.Close();
+    }
+}
+class Result
+{
+
+    /*
+     * Complete the 'gradingStudents' function below.
+     *
+     * The function is expected to return an INTEGER_ARRAY.
+     * The function accepts INTEGER_ARRAY grades as parameter.
+     */
+
+    public static List<int> gradingStudents(List<int> grades)
+    {
+        for (var i = 0; i < grades.Count; i++)
+        {
+            if (grades[i] >= 38)
+            {
+                var currDiff = 5 - (grades[i] % 5);
+                if (currDiff < 3)
+                {
+                    grades[i] += currDiff;
+                }
+            }
+        }
+
+        return grades;
+    }
+
+}
---
+++ MiniMaxSum/MiniMaxSum.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using static System.Console;
+using System.Linq;
+
+namespace MiniMaxSum;
+
+public class MiniMaxSum
+{
+    public static void Main(string[] args)
+    {
+
+        List<int> arr = Console.ReadLine().TrimEnd().Split(' ').ToList().Select(arrTemp => Convert.ToInt32(arrTemp)).ToList();
+
+        Result.miniMaxSum(arr);
+    }
+}
+
+class Result
+{
+
+    /*
+     * Complete the 'miniMaxSum' function below.
+     *
+     * The function accepts INTEGER_ARRAY arr as parameter.
+     */
+
+    public static void miniMaxSum(List<int> arr)
+    {
+        long sum = 0;
+        long min = long.MaxValue;
+        long max = long.MinValue;
+
+        for (var i = 0; i < 5; i++)
+        {
+            sum += arr[i];
+            min = Math.Min(min, arr[i]);
+            max = Math.Max(max, arr[i]);
+        }
+        WriteLine($"{sum - max} {sum - min}");
+    }
+
+}
+
