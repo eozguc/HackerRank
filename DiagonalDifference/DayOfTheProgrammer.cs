104487c749e7616de47d66d68a7e6db5a2c537ac
Erhan Özgüç <eozguc@hotmail.com>
2022-05-31 14:03:20 +0300

    Add files via upload

---
+++ DiagonalDifference/DayOfTheProgrammer.cs
@@ -0,0 +1,81 @@
+﻿
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
+
+
+namespace DiagonalDifference;
+
+public class DayOfTheProgrammer
+{
+    public static void Main(string[] args)
+    {
+        TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
+
+        int year = Convert.ToInt32(Console.ReadLine().Trim());
+
+        string result = Result.dayOfProgrammer(year);
+
+        textWriter.WriteLine(result);
+
+        textWriter.Flush();
+        textWriter.Close();
+    }
+}
+
+class Result
+{
+
+    /*
+     * Complete the 'dayOfProgrammer' function below.
+     *
+     * The function is expected to return a STRING.
+     * The function accepts INTEGER year as parameter.
+     */
+
+    public static string dayOfProgrammer(int year)
+    {
+        string s = year.ToString();
+        if (year == 1918)
+        {
+            return "26.09." + s;
+            
+        }
+        else if (year<=1917)
+        {
+            if (year % 4 == 0)
+            {
+                return "12.09." + s;
+                
+            }
+            else
+            {
+                return "13.09." + s;
+            }
+                
+        }
+        else
+        {
+            if (year % 400 == 0 || (year % 4 == 0) && year % 100 != 0)
+            {
+                return "12.09." + s;
+            }
+            else
+            {
+                return "13.09." + s;
+            }
+        }
+        
+       
+    }
+    
+
+}
+
