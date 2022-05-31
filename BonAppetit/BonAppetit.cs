40145e67a152d7f18783f23a6c5b5e88434d4762
Erhan Özgüç <eozguc@hotmail.com>
2022-05-31 14:28:11 +0300

    Add files via upload

---
+++ BonAppetit/BonAppetit.cs
@@ -0,0 +1,56 @@
+﻿
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
+
+namespace BonAppetit;
+
+public class BonAppetit
+{
+    public static void Main(string[] args)
+    {
+        string[] firstMultipleInput = Console.ReadLine().TrimEnd().Split(' ');
+
+        int n = Convert.ToInt32(firstMultipleInput[0]);
+
+        int k = Convert.ToInt32(firstMultipleInput[1]);
+
+        List<int> bill = Console.ReadLine().TrimEnd().Split(' ').ToList().Select(billTemp => Convert.ToInt32(billTemp)).ToList();
+
+        int b = Convert.ToInt32(Console.ReadLine().Trim());
+
+        Result.bonAppetit(bill, k, b);
+    }
+}
+
+class Result
+{
+
+    /*
+     * Complete the 'bonAppetit' function below.
+     *
+     * The function accepts following parameters:
+     *  1. INTEGER_ARRAY bill
+     *  2. INTEGER k
+     *  3. INTEGER b
+     */
+
+    public static void bonAppetit(List<int> bill, int k, int b)
+    {
+       
+        
+        bill.RemoveAt(k);
+        int actual = bill.Sum() / 2;
+        if (actual == b) Console.WriteLine("Bon Appetit");
+        else Console.WriteLine(b - actual);
+    }
+
+}
+
